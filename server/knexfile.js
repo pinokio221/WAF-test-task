e947c6850d2341364b0a13e4cdfb03c6980efc5a
pinokio <pinokio221b@gmail.com>
2021-05-23 22:12:23 +0300

    Update knexfile.js

--- server/knexfile.js
+++ server/knexfile.js
@@ -8,9 +8,9 @@ const knex = require('knex')({
         user : process.env.DATABASE_USER,
         password : process.env.DATABASE_PASSWORD,
         database : process.env.DATABASE_NAME,
-        //port: process.env.DATABASE_PORT,
+        port: process.env.DATABASE_PORT,
         charset : 'utf8mb4',
-        //ssl: { rejectUnauthorized: false }
+        ssl: { rejectUnauthorized: false }
     },
     migrations: {
         tableName: 'knex_migrations'
